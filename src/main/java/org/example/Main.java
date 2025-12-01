5e05e8108df70a83c6b8574d3bed11d4fe928924
sarahdz1989 <sarahstar.h279@gmail.com>
2025-11-30 20:06:06 -0600

    Sinonimos y antonimos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1487,7 +1487,12 @@ public class Main {
         contadorFallos = 0;
         for (int i = 0; i < palabrasFalladas.length; i++) palabrasFalladas[i] = -1;
         bienvenida();
-        System.out.println("En cada ronda, usted deberá:\n1- Escribir el significado en español de una palabra que se muestre en inglés,\n2- Escribir la palabra en inglés a partir de su significado en español, o\n3- Seleccionar la traducción correcta entre varias opciones.\nAdemás:\nCada palabra incluirá ejemplos contextualizados, como frases o fragmentos de conversación, para ayudarle a comprender y usar correctamente el vocabulario.\nRecibirá retroalimentación inmediata en cada intento, y al finalizar un nivel o categoría, podrá revisar un resumen con sus aciertos y errores.\nLas palabras que no domine serán retomadas automáticamente por el sistema en repaso.");
+        System.out.println("En este juego, deberás escribir un sinónimo o un antónimo.\n" +
+                "El sistema elegirá aleatoriamente qué tipo de respuesta debes dar.\n" +
+                "Verás una palabra y deberás escribir:\n" +
+                "- un sinónimo (misma idea), o\n" +
+                "- un antónimo (idea opuesta).\n" +
+                "Todas tus respuestas deben ser palabras aprendidas en la modalidad tradicional.\n");
         contador();
 
         // PRIMERA RONDA
